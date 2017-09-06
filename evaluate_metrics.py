67af78e669160e2238803cc3712cc418874f3db1
Matias Mattamala <mmattamala@ing.uchile.cl>
2017-09-06 01:47:10 -0300

    First commit

---
+++ evaluate_metrics.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+"""
+
+This script computes several SLAM metrics.
+It is based on the TUM scripts
+
+"""
+
+import sys
+import numpy as np
+import argparse
+import tum_utils
+import slam_metrics
+
+
+if __name__=="__main__":
+    # parse command line
+    parser = argparse.ArgumentParser(description='''
+    This script computes different error metrics for SLAM from the ground truth trajectory and the estimated trajectory.
+    ''')
+    # Add argument options
+    parser.add_argument('gt_file', help='ground truth trajectory (format: timestamp tx ty tz qx qy qz qw)')
+    parser.add_argument('est_file', help='estimated trajectory (format: timestamp tx ty tz qx qy qz qw)')
+    parser.add_argument('--offset', help='time offset added to the timestamps of the second file (default: 0.0)',default=0.0)
+    parser.add_argument('--scale', help='scaling factor for the second trajectory (default: 1.0)',default=1.0)
+    parser.add_argument('--max_difference', help='maximally allowed time difference for matching entries (default: 0.02)',default=0.02)
+    parser.add_argument('--delta', help='delta for evaluation (default: 1.0)',default=1.0)
+    parser.add_argument('--delta_unit', help='unit of delta (options: \'s\' for seconds, \'m\' for meters, \'rad\' for radians, \'f\' for frames; default: \'s\')',default='s')
+    #parser.add_argument('--save', help='save aligned second trajectory to disk (format: stamp2 x2 y2 z2)')
+    #parser.add_argument('--save_associations', help='save associated first and aligned second trajectory to disk (format: stamp1 x1 y1 z1 stamp2 x2 y2 z2)')
+    #parser.add_argument('--plot', help='plot the first and the aligned second trajectory to an image (format: png)')
+    #parser.add_argument('--verbose', help='print all evaluation data (otherwise, only the RMSE absolute translational error in meters after alignment will be printed)', action='store_true')
+    args = parser.parse_args()
+
+    # read files in TUM format or TUM modified format (with covariances)
+    gt_list  = tum_utils.read_file_list(args.gt_file)
+    est_list = tum_utils.read_file_list(args.est_file)
+
+    # check file format, we use the first element
+    gt_n_elements = len(gt_list[gt_list.keys()[0]])
+    est_n_elements = len(est_list[est_list.keys()[0]])
+
+    if(gt_n_elements != est_n_elements):
+        sys.exit("The format of both files is different, please check the input files")
+    if gt_n_elements == 7:
+        dataset_format = 'TUM'
+    elif gt_n_elements == 28:
+        dataset_format = 'TUM_mod'
+    else:
+        sys.exit("The format of files don't match any supported, please check the input files")
+
+    # associate sequences according to timestamps
+    matches = tum_utils.associate(gt_list, est_list, float(args.offset), float(args.max_difference))
+    if len(matches)<2:
+        sys.exit("Couldn't find matching timestamp pairs between groundtruth and estimated trajectory! Did you choose the correct sequence?")
+
+    #print(gt_list)
+    #print(est_list)
+
+    # generate numpy arrays for transformations, positions, and orientations
+    # generate positions as a 3 x N matrix, where N is the number of poses
+    gt_xyz  = np.matrix([[float(value) for value in gt_list[a][0:3]] for a,b in matches]).transpose()
+    est_xyz = np.matrix([[float(value)*float(args.scale) for value in est_list[b][0:3]] for a,b in matches]).transpose()
+
+    # generate orientations as a 4 x N matrix
+    gt_quat  = np.matrix([[float(value) for value in gt_list[a][3:7]] for a,b in matches]).transpose()
+    est_quat = np.matrix([[float(value) for value in est_list[b][3:7]] for a,b in matches]).transpose()
+
+    # generate poses as a N x 4x4 dict
+    gt_pose  = dict( [ (a,tum_utils.transform44(np.array(gt_list[a][0:7]))) for a,b in matches ] )
+    est_pose = dict( [ (b,tum_utils.transform44(np.array(est_list[b][0:7]))) for a,b in matches ] )
+
+    # if available, generate covariances as N x 6x6 dict
+    if dataset_format == 'TUM_mod':
+        gt_pose  = dict( [ (a,tum_utils.covariance66(np.array(gt_list[a][7:]))) for a,b in matches ] )
+        est_pose = dict( [ (b,tum_utils.covariance66(np.array(est_list[b][7:]))) for a,b in matches ] )
+
+    # Compute metrics
+    # ATE (Absolute trajectory error)
+    ate_horn_error, ate_horn_rot, ate_horn_trans, ate_horn_scale = slam_metrics.ATE_Horn(gt_xyz, est_xyz, show=True)
+    slam_metrics.compute_statistics_per_axis(ate_horn_error)
+    slam_metrics.compute_statistics_absolute(ate_horn_error, variable='translational')
+
+    # ATE (Absolute trajectory error, SE(3))
+    ate_se3_error = slam_metrics.ATE_SE3(gt_pose, est_pose, matches=matches, show=True)
+    slam_metrics.compute_statistics_per_axis(ate_se3_error)
+    slam_metrics.compute_statistics_absolute(ate_se3_error[0:3,:], variable='translational')
+    slam_metrics.compute_statistics_absolute(ate_se3_error[3:6,:], variable='rotational')
+
+    # RPE (Relative Pose Error)
+    rpe_error, rpe_trans_error, rpe_rot_error = slam_metrics.RPE(gt_pose, est_pose, args.delta, show=True)
+    slam_metrics.compute_statistics_per_axis(rpe_error)
+    slam_metrics.compute_statistics_absolute(rpe_error[0:3,:], variable='translational')
+    slam_metrics.compute_statistics_absolute(rpe_error[3:6,:], variable='rotational')
---
+++ slam_metrics.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Implementation of main metrics used in Visual SLAM
+'''
+
+import random
+import numpy as np
+import SE3UncertaintyLib as SE3Lib
+import tum_utils
+
+def compute_statistics_per_axis(err, show=True):
+    """
+    Computes the mean, RMSE, standard deviation, median, min and max
+
+    @param err: a MxN np array.
+    M is the number of components by sample (M=3 if SO(3), M=6 if SE(3)). N is the number of samples.
+
+    """
+    stats = {}
+
+    dimensions = ['x', 'y', 'z', 'roll', 'pitch', 'yaw']
+    units = ['m', 'm', 'm', 'rad', 'rad', 'rad']
+
+    (M,N) = err.shape
+
+    # RMSE
+    stats['rmse'] = np.sqrt(np.sum(np.multiply(err, err), axis=1) / N)
+    # Mean
+    stats['mean'] = np.mean(err, axis=1) # computed by column
+    # Standard Deviation
+    stats['std']  = np.std(err, axis=1) # computed by column
+    # Median
+    stats['median'] = np.median(err, axis=1) # computed by column
+    # Min
+    stats['min'] = np.min(np.fabs(err), axis=1) # computed by column
+    # Max
+    stats['max'] = np.max(err, axis=1) # computed by column
+
+    if show:
+        for key in stats:
+            print(key)
+            for dim,err,unit in zip(dimensions, stats[key], units):
+                print('   %s-axis: %f [%s]' % (dim, err, unit))
+
+    return stats
+
+def compute_statistics_absolute(err, show=True, variable='translational'):
+    """
+    Computes the mean, RMSE, standard deviation, median, min and max
+
+    @param err: a MxN np array.
+    M is the number of components by sample (M=3 if SO(3), M=6 if SE(3)). N is the number of samples.
+
+    """
+    stats = {}
+
+    (M,N) = err.shape
+
+    trans_error = np.sqrt(np.sum(np.multiply(err,err),0)).A[0]
+
+    # RMSE
+    stats['rmse'] = np.sqrt(np.dot(trans_error,trans_error) / len(trans_error))
+    # Mean
+    stats['mean'] = np.mean(trans_error)
+    # Standard Deviation
+    stats['std']  = np.std(trans_error)
+    # Median
+    stats['median'] = np.median(trans_error)
+    # Min
+    stats['min'] = np.min(trans_error)
+    # Max
+    stats['max'] = np.max(trans_error)
+
+    if show:
+        for key in stats:
+            print('absolute %s error: %s: %f [m]' % (variable, key, stats[key]))
+
+    return stats
+
+
+def ATE_SE3(traj_gt, traj_est, show=True, matches=None, offset=0.0, max_difference=0.02):
+    """
+    This method computes the Absolute Trajectory Error (ATE)
+    Ref: Sturm et al. (2012)
+
+    @param estimated: a dictionary of matrices representing estimated poses
+    @param ground_truth: a dictionary with real poses
+
+    """
+    if show:
+        print('\nATE: Absolute Trajectory Error, SE(3) error')
+
+    if matches is None:
+        matches = tum_utils.associate(traj_gt, traj_est)
+        if len(matches)<2:
+            sys.exit("Couldn't find matching timestamp pairs between groundtruth and estimated trajectory! Did you choose the correct sequence?")
+
+    errors = np.matrix([SE3Lib.TranToVec(tum_utils.transform_diff(traj_gt[a], traj_est[b])) for a,b in matches]).transpose()
+
+    return errors
+
+def ATE_Horn(traj_gt, traj_est, compute_scale=False, show=True):
+    """Align two trajectories using the method of Horn (closed-form).
+    It includes the automatic scale recovery modification by Raul Mur-Artal
+
+    Input:
+    traj_xyz_gt -- first trajectory (3xn)
+    traj_xyz_est -- second trajectory (3xn)
+
+    Output:
+    rot -- rotation matrix (3x3)
+    trans -- translation vector (3x1)
+    trans_error -- translational error per point (1xn)
+
+    """
+
+    if show:
+        print('\nATE: Absolute Trajectory Error - Horn alignment')
+
+    #np.set_printoptions(precision=3,suppress=True)
+    traj_gt_zerocentered = traj_gt - traj_gt.mean(1)
+    traj_est_zerocentered = traj_est - traj_est.mean(1)
+
+    W = np.zeros( (3,3) )
+    for column in range(traj_gt.shape[1]):
+        W += np.outer(traj_gt_zerocentered[:,column],traj_est_zerocentered[:,column])
+    U,d,Vh = np.linalg.linalg.svd(W.transpose())
+    S = np.matrix(np.identity( 3 ))
+    if(np.linalg.det(U) * np.linalg.det(Vh)<0):
+        S[2,2] = -1
+    rot = U*S*Vh
+
+    s = 1.0
+    if compute_scale:
+        rottraj_gt = rot*traj_gt_zerocentered
+        dots = 0.0
+        norms = 0.0
+
+        for column in range(traj_est_zerocentered.shape[1]):
+    	    dots += np.dot(traj_est_zerocentered[:,column].transpose(),rottraj_gt[:,column])
+            normi = np.linalg.norm(traj_gt_zerocentered[:,column])
+            norms += normi*normi
+
+        s = float(dots/norms)
+
+    #print "scale: %f " % s
+    trans = traj_est.mean(1) - s*rot * traj_gt.mean(1)
+
+    traj_gt_aligned = s*rot * traj_gt + trans
+    alignment_error = traj_gt_aligned - traj_est
+
+    #trans_error = np.sqrt(np.sum(np.multiply(alignment_error,alignment_error),0)).A[0]
+
+    return alignment_error, rot, trans, s
+
+def RPE(traj_gt, traj_est, delta, param_max_pairs=10000, param_fixed_delta=False, param_delta=1.00, param_delta_unit="s", param_offset=0.00, param_scale=1.00, show=True):
+    """
+    This method computes the Relative Pose Error (RPE)
+    Ref: Sturm et al. (2012)
+
+    Input:
+    traj_gt -- the first trajectory (ground truth)
+    traj_est -- the second trajectory (estimated trajectory)
+    param_max_pairs -- number of relative poses to be evaluated
+    param_fixed_delta -- false: evaluate over all possible pairs
+                         true: only evaluate over pairs with a given distance (delta)
+    param_delta -- distance between the evaluated pairs
+    param_delta_unit -- unit for comparison:
+                        "s": seconds
+                        "m": meters
+                        "rad": radians
+                        "deg": degrees
+                        "f": frames
+    param_offset -- time offset between two trajectories (to traj_xyz_gt the delay)
+    param_scale -- scale to be applied to the second trajectory
+
+    Output:
+    list of compared poses and the resulting translation and rotation error
+    """
+
+    if show:
+        print('\nRPE: Relative Pose Error')
+
+    stamps_gt = list(traj_gt.keys())
+    stamps_est = list(traj_est.keys())
+    stamps_gt.sort()
+    stamps_est.sort()
+
+    stamps_est_return = []
+    for t_est in stamps_est:
+        t_gt = stamps_gt[tum_utils.find_closest_index(stamps_gt,t_est + param_offset)]
+        t_est_return = stamps_est[tum_utils.find_closest_index(stamps_est,t_gt - param_offset)]
+        t_gt_return = stamps_gt[tum_utils.find_closest_index(stamps_gt,t_est_return + param_offset)]
+        if not t_est_return in stamps_est_return:
+            stamps_est_return.append(t_est_return)
+    if(len(stamps_est_return)<2):
+        raise Exception("Number of overlap in the timestamps is too small. Did you run the evaluation on the right files?")
+
+    if param_delta_unit=="s":
+        index_est = list(traj_est.keys())
+        index_est.sort()
+    elif param_delta_unit=="m":
+        index_est = tum_utils.distances_along_trajectory(traj_est)
+    elif param_delta_unit=="rad":
+        index_est = tum_utils.rotations_along_trajectory(traj_est,1)
+    elif param_delta_unit=="deg":
+        index_est = tum_utils.rotations_along_trajectory(traj_est,180/np.pi)
+    elif param_delta_unit=="f":
+        index_est = range(len(traj_est))
+    else:
+        raise Exception("Unknown unit for delta: '%s'"%param_delta_unit)
+
+    if not param_fixed_delta:
+        if(param_max_pairs==0 or len(traj_est)<np.sqrt(param_max_pairs)):
+            pairs = [(i,j) for i in range(len(traj_est)) for j in range(len(traj_est))]
+        else:
+            pairs = [(random.randint(0,len(traj_est)-1),random.randint(0,len(traj_est)-1)) for i in range(param_max_pairs)]
+    else:
+        pairs = []
+        for i in range(len(traj_est)):
+            j = tum_utils.find_closest_index(index_est,index_est[i] + param_delta)
+            if j!=len(traj_est)-1:
+                pairs.append((i,j))
+        if(param_max_pairs!=0 and len(pairs)>param_max_pairs):
+            pairs = random.sample(pairs,param_max_pairs)
+
+    gt_interval = np.median([s-t for s,t in zip(stamps_gt[1:],stamps_gt[:-1])])
+    gt_max_time_difference = 2*gt_interval
+
+    result = []
+    diff_pose = []
+    for i,j in pairs:
+        stamp_est_0 = stamps_est[i]
+        stamp_est_1 = stamps_est[j]
+
+        stamp_gt_0 = stamps_gt[ tum_utils.find_closest_index(stamps_gt,stamp_est_0 + param_offset) ]
+        stamp_gt_1 = stamps_gt[ tum_utils.find_closest_index(stamps_gt,stamp_est_1 + param_offset) ]
+
+        if(abs(stamp_gt_0 - (stamp_est_0 + param_offset)) > gt_max_time_difference  or
+           abs(stamp_gt_1 - (stamp_est_1 + param_offset)) > gt_max_time_difference):
+            continue
+
+        error44 = tum_utils.transform_diff(  tum_utils.scale(
+                           tum_utils.transform_diff( traj_est[stamp_est_1], traj_est[stamp_est_0] ), param_scale),
+                           tum_utils.transform_diff( traj_gt[stamp_gt_1], traj_gt[stamp_gt_0] ) )
+        diff_pose.append(error44)
+
+        trans = tum_utils.compute_distance(error44)
+        rot = tum_utils.compute_angle(error44)
+
+        result.append([stamp_est_0,stamp_est_1,stamp_gt_0,stamp_gt_1,trans,rot])
+
+    if len(result)<2:
+        raise Exception("Couldn't find matching timestamp pairs between groundtruth and estimated trajectory!")
+
+    stamps = np.array(result)[:,0]
+    trans_error = np.array(result)[:,4]
+    rot_error = np.array(result)[:,5]
+
+    errors = np.matrix([SE3Lib.TranToVec(dT) for dT in diff_pose]).transpose()
+
+    return errors, trans_error, rot_error
